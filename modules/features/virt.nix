903e6de44dde14d263dde9117afab15b6e81350d
Andrew <45006345+Avakulko@users.noreply.github.com>
2026-07-21 19:11:16 +0500

    feat: add virtiofs

--- modules/features/virt.nix
+++ modules/features/virt.nix
@@ -1,8 +1,14 @@
 {
-  flake.nixosModules.virt = {
-    virtualisation.podman.enable = true;
-    virtualisation.libvirtd.enable = true;
-    programs.virt-manager.enable = true;
-    users.users.andrew.extraGroups = ["libvirtd" "kvm"];
+  flake = {
+    nixosModules.virt = {pkgs, ...}: {
+      virtualisation.podman.enable = true;
+
+      virtualisation.libvirtd = {
+        enable = true;
+        qemu.vhostUserPackages = [pkgs.virtiofsd];
+      };
+      programs.virt-manager.enable = true;
+      users.users.andrew.extraGroups = ["libvirtd" "kvm"];
+    };
   };
 }
