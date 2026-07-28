cf0958f66dc09f2a73c5f37552db73a502bb18a4
Andrew <45006345+Avakulko@users.noreply.github.com>
2026-07-28 22:01:47 +0500

    feat: configure niri

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -1,37 +1,65 @@
 {
-  self,
   inputs,
+  moduleWithSystem,
   ...
 }: {
-  flake.nixosModules.niri = {
+  flake.nixosModules.niri = moduleWithSystem ({
     pkgs,
-    lib,
+    self',
     ...
   }: {
     programs.niri = {
       enable = true;
-      # package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+      package = self'.packages.niri.override {
+        libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
+          version = "0.3.0";
+          src = pkgs.fetchFromGitLab {
+            domain = "gitlab.freedesktop.org";
+            owner = "emersion";
+            repo = "libdisplay-info";
+            rev = finalAttrs.version;
+            sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+          };
+        });
+      };
+      useNautilus = true;
     };
     environment.systemPackages = with pkgs; [
       xwayland-satellite
-      tela-circle-icon-theme
     ];
+  });
+  perSystem = {
+    pkgs,
+    lib,
+    self',
+    ...
+  }: {
+    packages.niri = inputs.wrapper-modules.wrappers.niri.wrap {
+      inherit pkgs;
+      settings = {
+        prefer-no-csd = _: {};
+        # layout = {
+        # focus-ring = {};
+        # };
+        binds = {
+          "Mod+Return".spawn-sh = lib.getExe self'.packages.kitty;
+          "Mod+F".maximize-column = _: {};
+
+          "Mod+H".focus-column-left = _: {};
+          "Mod+Shift+H".move-column-left = _: {};
+
+          "Mod+L".focus-column-right = _: {};
+          "Mod+Shift+L".move-column-right = _: {};
+
+          "Mod+J".focus-workspace-down = _: {};
+          "Mod+Shift+J".move-column-to-workspace-down = _: {};
+
+          "Mod+K".focus-workspace-up = _: {};
+          "Mod+Shift+K".move-column-to-workspace-up = _: {};
+
+          "Mod+Q".close-window = _: {};
+        };
+      };
+    };
   };
-  # perSystem =
-  # {
-  # pkgs,
-  # lib,
-  # self',
-  # ...
-  # }:
-  # {
-  # packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
-  # inherit pkgs;
-  # settings = {
-  # 	binds = {
-  # 		"Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
-  # 	};
-  # };
-  # };
-  # };
 }
