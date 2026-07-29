9e5c3d647c3b7e4d9582746e7c0d11a85d3a719b
Andrew <45006345+Avakulko@users.noreply.github.com>
2026-07-29 20:22:13 +0500

    chore: add notes about vm-curator

--- modules/features/virt.nix
+++ modules/features/virt.nix
@@ -9,6 +9,12 @@
       };
       programs.virt-manager.enable = true;
       users.users.andrew.extraGroups = ["libvirtd" "kvm"];
+      #  BUG: vm-curator fails with no such file or directory /usr/share/OVMF/OVMF_CODE.fd
+
+      # environment.systemPackages = with pkgs; [
+      #   OVMF
+      #   edk2-uefi-shell
+      # ];
     };
   };
 }
