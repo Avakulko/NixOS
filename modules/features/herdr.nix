fd3d7234082a6b8708a90e559b8560c259ad5727
Andrew <45006345+Avakulko@users.noreply.github.com>
2026-08-03 13:44:58 +0500

    feat: configure herdr

--- modules/features/herdr.nix
+++ modules/features/herdr.nix
@@ -2,7 +2,6 @@
   flake.nixosModules.herdr = {pkgs, ...}: {
     # TODO: declarative config
     environment.systemPackages = [
-      pkgs.jq # Needed for https://github.com/paulbkim-dev/vim-herdr-navigation
       pkgs.herdr
     ];
   };
