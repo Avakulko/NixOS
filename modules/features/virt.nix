1144596abef9c3878dcf4b5623295e62e182426a
Andrew <45006345+Avakulko@users.noreply.github.com>
2026-08-06 11:18:15 +0500

    feat: switch to docker

--- modules/features/virt.nix
+++ modules/features/virt.nix
@@ -1,14 +1,16 @@
 {
   flake = {
     nixosModules.virt = {pkgs, ...}: {
-      virtualisation.podman.enable = true;
+      # virtualisation.podman.enable = true;
+      virtualisation.docker.enable = true;
+      environment.systemPackages = [pkgs.docker-compose];
 
       virtualisation.libvirtd = {
         enable = true;
         qemu.vhostUserPackages = [pkgs.virtiofsd];
       };
       programs.virt-manager.enable = true;
-      users.users.andrew.extraGroups = ["libvirtd" "kvm"];
+      users.users.andrew.extraGroups = ["libvirtd" "kvm" "docker"];
       #  BUG: vm-curator fails with no such file or directory /usr/share/OVMF/OVMF_CODE.fd
 
       # environment.systemPackages = with pkgs; [
